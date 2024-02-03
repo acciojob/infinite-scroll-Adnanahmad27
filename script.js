5ddad717919c6b639c36eeb4ee10f1d8a5aadd03
Adnan Ahmad <98473885+Adnanahmad27@users.noreply.github.com>
2024-02-04 03:59:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
-//your code here!
-
+const container = document.querySelector("#infi-list");
+let itemCnt = 1;
+container.addEventListener("scroll" , () => {
+	let remaining = container.scrollHeight - container.clientHeight - container.scrollTop;
+	if(remaining <= 1 ){
+		generateElements(2);
+	}
+});
+generateElements(10);
+function generateElements(n) {
+	for(let i=0 ; i<n;i++){
+		const inner = document.createElement("li");
+		//const inner = document.createElement("div");
+		inner.innerText = `Item ${itemCnt++}`;
+		inner.style.height = "50px";
+		//ele.appendChild(inner);
+		container.appendChild(inner);
+	}
+}
